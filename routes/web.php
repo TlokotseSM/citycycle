ebb05d7d91cb61904d67c8dcf19fec31c3cf0a47
TlokotseSM <tlokotsemogudi@gmail.com>
2025-06-10 11:43:36 +0200

    Initialized route configuration in routes/web.php for improved navigation and functionality.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,30 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HubController;
+use App\Http\Controllers\BicycleController;
+use App\Http\Controllers\ReservationController;
+use App\Http\Controllers\MaintenanceController;
 
-Route::get('/', function () {
-    return view('welcome');
+Auth::routes();
+
+Route::get('/', [HubController::class, 'index'])->name('home');
+
+Route::resource('hubs', HubController::class)->only(['index', 'show']);
+
+Route::get('/bicycles/maintenance', [BicycleController::class, 'forMaintenance'])
+    ->name('bicycles.maintenance')
+    ->middleware('auth');
+
+Route::post('/bicycles/{bicycle}/maintenance', [MaintenanceController::class, 'updateStatus'])
+    ->name('bicycles.maintenance.update')
+    ->middleware('auth');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/bicycles/{bicycle}/reserve', [ReservationController::class, 'create'])
+        ->name('bicycles.reserve');
+    Route::post('/reservations', [ReservationController::class, 'store'])
+        ->name('reservations.store');
+    Route::get('/reservations/{reservation}', [ReservationController::class, 'show'])
+        ->name('reservations.show');
 });
